03d081398b9949816d14fdd960eb087d3c35ab5f
kvqn <guneetaggarwal@gmail.com>
2024-07-22 23:29:14 +0530

    heartbeat will use hostname

--- scripts/heartbeat/main.py
+++ scripts/heartbeat/main.py
@@ -1,8 +1,11 @@
 from friday_logger import Logger
 from dotenv import load_dotenv
 from os import getenv
+import subprocess
 
 load_dotenv()
+
+hostname = subprocess.run(["hostname"], capture_output=True).stdout
 
 FRIDAY_ENDPOINT = getenv("FRIDAY_ENDPOINT")
 assert FRIDAY_ENDPOINT is not None
@@ -12,4 +15,4 @@ FRIDAY_TOPIC = getenv("FRIDAY_TOPIC")
 
 logger = Logger(FRIDAY_ENDPOINT)
 
-logger.info("Server heartbeat")
+logger.info(f"Heartbeat ({hostname})")
