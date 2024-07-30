a033915ef89265fdc7b161d5cebf2b71f555da38
kvqn <guneetaggarwal@gmail.com>
2024-07-30 22:08:58 +0530

    add tailwind and prettier

---
+++ servers/dashboard/frontend/prettier.config.js
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  semi: false,
+}
+
+// eslint-disable-next-line no-undef
+module.exports = config
